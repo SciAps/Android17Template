e461decfe93ff8ea3ddf9175c766bdbcd619cd1f
Stephen Gowen <dev.sgowen@gmail.com>
2018-11-29 16:58:43 -0500

    Adding Sciaps fork of GraphView to template project.

--- app/src/main/java/sciaps/com/android17template/MainActivity.java
+++ app/src/main/java/sciaps/com/android17template/MainActivity.java
@@ -4,6 +4,10 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
 public class MainActivity extends Activity
 {
 
@@ -22,6 +26,16 @@ public class MainActivity extends Activity
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
         tv.setText(stringFromJNI());
+
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        graph.addSeries(series);
     }
 
     /**
